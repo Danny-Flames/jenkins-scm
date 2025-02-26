d8ff5e0d25f61fe5af31e9be14f44246154eae71
Danny-Flames <danniskolobosiky@gmail.com>
2025-02-26 01:45:45 +0100

    latest changes-5

--- app.js
+++ app.js
@@ -6,3 +6,5 @@ console.log('We updated the jenkins system config')
 console.log('We updated the jenkins system config to check the first pipeline-job')
 
 console.log('We updated the jenkins system config to check the first pipeline-job-2')
+
+console.log('We updated the jenkins system config to check the first pipeline-job-5')
