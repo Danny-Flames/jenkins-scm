63d0b6a5cc0e75579bf041d359c6acbc6434dc36
Danny-Flames <danniskolobosiky@gmail.com>
2025-02-26 01:39:16 +0100

    latest changes early morning

--- app.js
+++ app.js
@@ -4,3 +4,5 @@ console.log('Hello..')
 console.log('We updated the jenkins system config')
 
 console.log('We updated the jenkins system config to check the first pipeline-job')
+
+console.log('We updated the jenkins system config to check the first pipeline-job-2')
